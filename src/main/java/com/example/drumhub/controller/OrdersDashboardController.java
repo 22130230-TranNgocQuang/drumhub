cbc85f665f51bb097a6ffad56cdea9ad034b1f85
Nguyen Huu Nam <22130174@st.hcmuaf.edu.vn>
2025-06-03 15:56:23 +0700

    sửa lại phương thức doGet về việc lấy dẫn tới trang detail của danh sách Orders ở dashboard

--- src/main/java/com/example/drumhub/controller/OrdersDashboardController.java
+++ src/main/java/com/example/drumhub/controller/OrdersDashboardController.java
@@ -1,6 +1,8 @@
 package com.example.drumhub.controller;
 
 import com.example.drumhub.dao.models.Order;
+import com.example.drumhub.dao.models.OrderDetailItem;
+import com.example.drumhub.dao.models.User;
 import com.example.drumhub.services.OrdersDashboardService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
@@ -9,9 +11,12 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "OrdersDashboardController", value = "/dashboard/orders")
+@WebServlet(name = "OrdersDashboardController", urlPatterns = {
+        "/dashboard/order",            // Danh sách đơn hàng
+        "/dashboard/order/detail"      // Chi tiết đơn hàng
+})
 public class OrdersDashboardController extends HttpServlet {
-    private OrdersDashboardService service;
+    private OrdersDashboardService service ;
 
     @Override
     public void init() throws ServletException {
@@ -20,8 +25,33 @@ public class OrdersDashboardController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getServletPath();
+
+        if ("/dashboard/order/detail".equals(path)) {
+            showOrderDetail(request, response);
+        } else {
+            listOrders(request, response);
+        }
+    }
+
+    private void listOrders(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Order> orders = service.getAll();
         request.setAttribute("orders", orders);
-        request.getRequestDispatcher("/dashboard/orders/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/dashboard/order/index.jsp").forward(request, response);
     }
+
+    private void showOrderDetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int orderId = Integer.parseInt(request.getParameter("id"));
+
+        Order order = service.getById(orderId);
+        User user = service.getUserByOrderId(orderId);
+        List<OrderDetailItem> items = service.getOrderDetailsByOrderId(orderId);
+
+        request.setAttribute("order", order);
+        request.setAttribute("user", user);
+        request.setAttribute("items", items);
+
+        request.getRequestDispatcher("/dashboard/order/detail.jsp").forward(request, response);
+    }
+
 }
