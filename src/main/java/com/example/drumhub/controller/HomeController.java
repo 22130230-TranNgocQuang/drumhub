adbd4794a9110b2d2376ee877a5feeeae2a9ddb8
22130243VoDaiThanhTai <22130243@st.hcmuaf.edu.vn>
2025-06-04 09:06:49 +0700

    xử lý Controller gọi đến hàm lấy sản phẩm từ service

--- src/main/java/com/example/drumhub/controller/HomeController.java
+++ src/main/java/com/example/drumhub/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.example.drumhub.controller;
 
+import com.example.drumhub.dao.ProductDAO;
+import com.example.drumhub.dao.models.Product;
+import com.example.drumhub.services.ProductService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +10,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/home")
 public class HomeController extends HttpServlet {
+
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // Load dữ liệu nếu cần (sản phẩm, bài viết, v.v.)
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+        ProductService service = new ProductService();
+        List<Product> topProducts = service.getListByN();
+        request.setAttribute("products", topProducts);
+        System.out.println("Số lượng sản phẩm lấy được: " + topProducts.size());
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
     }
 }
